2560b6eef51083cc13a7ca489c73f92c29eae001
knothhe <guanglaihe@gmail.com>
2019-02-12 14:59:57 +0800

    add ex-1.18 and update ex-1.17, (original ex-1.17 actually is ex-1.18)

--- SICP/ex-1.18.rkt
+++ SICP/ex-1.18.rkt
@@ -12,10 +12,13 @@
 ; a * b = 2*a * b/2, b is even
 ; a * b = a + a * (b - 1), b is odd
 ; a * 0 = 0
+; keep the expression a * b + n constant
 (define (product a b)
-  (cond ((= b 0) 0)
-        ((even? b) (product (double a) (havle b)))
-        (else (+ a (product a (- b 1))))))
+  (define (iter a b n)
+    (cond ((= b 0) n)
+          ((even? b) (iter (double a) (havle b) n))
+          (else (iter a (- b 1) (+ n a)))))
+  (iter a b 0))
 
 (product 2 4)
 (product 3 4)
