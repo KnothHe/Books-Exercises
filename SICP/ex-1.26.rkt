3da9bca36ff0825efcef4f4b77b4f1b29913eafa
knothhe <guanglaihe@gmail.com>
2019-02-12 21:18:17 +0800

    add ex-1.26

---
+++ SICP/ex-1.26.rkt
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define (square n) (* n n))
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (* (expmod base (/ exp 2) m)
+                       (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+; because (expmod (/ exp 2) m) evaluated two times
