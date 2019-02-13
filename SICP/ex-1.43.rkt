9570825397fcab1e6f9b7c296f9f15e6c36efb5a
knothhe <guanglaihe@gmail.com>
2019-02-13 21:41:42 +0800

    add ex-1.43

---
+++ SICP/ex-1.43.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+(define (square x) (* x x))
+(define (inc x) (+ x 1))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (< n 1)
+      (lambda (x) x)
+      (compose f (repeated f (- n 1)))))
+
+((repeated square 2) 5)
