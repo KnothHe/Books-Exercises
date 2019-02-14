0979ec8983e361340d00c0f656936d250420db5e
knothhe <guanglaihe@gmail.com>
2019-02-14 16:10:16 +0800

    update ex-2.6, correct answer

--- SICP/chapter-02/ex-2.6.rkt
+++ SICP/chapter-02/ex-2.6.rkt
@@ -5,10 +5,17 @@
 (define (add-1 n)
   (lambda (f) (lambda (x) (f ((n f) x)))))
 
-(define one (lambda (x) (f x)))
-(define two (lambda (x) (f (f x))))
+(define one
+  (lambda (f)
+    (lambda (x)
+      (f x))))
+(define two
+  (lambda (f)
+    (lambda (x)
+      (f (f x)))))
 
 (define (add a b)
-  (lambda (x)
-    ((a f) (b f) x)))
+  (lambda (f)
+    (lambda (x)
+    ((a f) ((b f) x)))))
 
