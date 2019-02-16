5e68a0183e2f322f251a04059f8838508c2528eb
knothhe <guanglaihe@gmail.com>
2019-02-16 12:40:22 +0800

    add ex-2.12

---
+++ SICP/chapter-02/ex-2.12.rkt
@@ -0,0 +1,32 @@
+#lang sicp
+
+(define (make-interval a b)
+  (cons a b))
+
+(define (lower-bound x)
+  (car x))
+
+(define (upper-bound x)
+  (cdr x))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent center percent)
+  (let ((width (* center percent)))
+    (make-center-width center width)))
+
+(define (percent x)
+  (/ (width x) (center x)))
+
+(define x (make-center-percent 3 0.05))
+(lower-bound x)
+(upper-bound x)
+(center x)
+(width  x)
+(percent x)
