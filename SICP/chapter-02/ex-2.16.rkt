2506f082f864c02c435085d5b530ab30a2c6fe49
knothhe <guanglaihe@gmail.com>
2019-02-16 14:20:05 +0800

    fix typos

--- SICP/chapter-02/ex-2.16.rkt
+++ SICP/chapter-02/ex-2.16.rkt
@@ -1,6 +1,6 @@
 #lang sicp
 
-; I think exercise 2.14 and exercise 2.15 are in the scope of numerical problems.
+; I think exercise 2.15 and exercise 2.16 are in the scope of numerical problems.
 ; I don't plan to (and really can't solve it) take the time to consider these two problems.
 
 
