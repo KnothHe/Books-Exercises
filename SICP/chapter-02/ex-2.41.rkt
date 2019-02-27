cbac98bae0635e0bce9f1f314963f5634d18e648
knothhe <guanglaihe@gmail.com>
2019-02-27 10:25:16 +0800

    add ex-2.41

---
+++ SICP/chapter-02/ex-2.41.rkt
@@ -0,0 +1,40 @@
+#lang sicp
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+;; ordered-triples-sum
+(define (ordered-triples n)
+  (flatmap (lambda (i)
+             (flatmap (lambda (j)
+                        (map (lambda (k) (list i j k))
+                             (enumerate-interval 1 (- j 1))))
+                      (enumerate-interval 1 (- i 1))))
+           (enumerate-interval 1 n)))
+
+(define (sum-of-triple triple)
+  (+ (car triple) (cadr triple) (caddr triple)))
+
+(define (ordered-triples-sum n s)
+  (filter (lambda (triple) (= (sum-of-triple triple) s))
+          (ordered-triples n)))
+
+(display (ordered-triples-sum 6 10))
