73cda783fad03fba57712cdbcc8c9539361908b9
knothhe <guanglaihe@gmail.com>
2019-03-09 13:29:37 +0800

    add ex-2.48

---
+++ SICP/chapter-02/ex-2.48.rkt
@@ -0,0 +1,11 @@
+#lang sicp
+
+;; definition of segment
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
