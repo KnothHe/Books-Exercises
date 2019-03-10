29397ab875c3017072377239d7696a1772754e38
knothhe <guanglaihe@gmail.com>
2019-03-10 10:33:47 +0800

    update ex-2.57

--- SICP/chapter-02/ex-2.57.rkt
+++ SICP/chapter-02/ex-2.57.rkt
@@ -1,10 +1,5 @@
 #lang sicp
 
-(define (memq item x)
-  (cond ((null? x) false)
-        ((eq? item (car x)) x)
-        (else (memq item (cdr x)))))
-
 (define (deriv exp var)
   (cond ((number? exp) 0)
         ((variable? exp)
